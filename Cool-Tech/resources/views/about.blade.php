a15394cc70e671523d1eb8640abbf2bdc002005b
CarstensMartin <carstensmartin@outlook.com>
2022-08-03 14:00:48 +0200

    Upload study progress Level 4

---
+++ Cool-Tech/resources/views/about.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Link a CSS file for the main layout of the page-->
+    <link href="/css/main.css" rel="stylesheet">
+    <title>About Us</title>
+</head>
+
+<body onload=displayAlertFooter()>
+    <br>
+    <!-- Button to home page -->
+    <button><a href="{{ url('/') }}">Return Home</a></button>
+    <div class="pageContent">
+        <h1>About page</h1>
+        <h2>What is an About Us page?</h2>
+        <p>
+            An About Us page is a home for your founding story, a place to show off your business wins, and a sales page that answers the most pressing question new customers have about your business, and why they should buy your products or service.<br><br>
+            The problem with most About Us pages is that they’re an afterthought—a link buried at the bottom of a site that leads to a few hastily written paragraphs about a company.<br><br>
+            What an About Us page should be is a goal-oriented sales page, one that focuses on highlighting the biggest selling points of your story and brand at the top of the page, making a strong first impression on curious customers<br><br>
+            In Shopify’s customer trust research, we found that shoppers navigate to an About Us page to learn more about the brand and the people behind the products. Your About Page should address those two curiosities shoppers have to help with decision making. <br><br>
+            Shoppers are also interested in a company’s mission. They’ll use the About Us page to determine if they share core values with the business and to decide if they want to shop with the business or not. <br><br>
+            In contrast to a landing page, your About Us page is the ideal place to accommodate a number of objectives:
+        </p>
+
+        <ul>
+            <li>Communicate the story of your business and why you started it.</li>
+            <li>Describe the customers or the cause that your business serves.</li>
+            <li>Explain your business model or how your products are made.</li>
+            <li>Put a face to your business, featuring the founders or the people on your team</li>
+            <li>Incorporate persuasive content (e.g., an explainer video, data visualizations, links to blog posts) that might otherwise clutter your homepage.</li>
+        </ul>
+        <!-- Text found at https://www.shopify.co.za/blog/how-to-write-an-about-us-page -->
+    </div>
+    <!-- Add pop up cookie and Footer -->
+    <div class="cookie">
+    </div>
+    <script type="text/javascript" src="/js/main.js"></script>
+</body>
+
+</html>
